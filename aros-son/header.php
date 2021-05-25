5ca925d7c0b582ce930fde889b757299bacf5d06
NicolasPenon <73479527+NicolasPenon@users.noreply.github.com>
2021-05-25 14:27:17 +0200

    sejt

--- aros-son/header.php
+++ aros-son/header.php
@@ -2,4 +2,5 @@
   <form action="account_list.php" method="GET">
   <input id="search" type="text" placeholder="Søg her">
   <input id="submit" type="submit" value="Search">
+  Skriver noget tilfældigt
 </header>
