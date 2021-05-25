c859171b0db90cf579a2411e9baa7a563fd8eaaa
NicolasPenon <73479527+NicolasPenon@users.noreply.github.com>
2021-05-25 16:13:45 +0200

    ændret header

--- aros-son/header.php
+++ aros-son/header.php
@@ -1,9 +1,15 @@
 <header>
   <form action="menu.php">
     <input id="menu" type="image" src="img/menu.jpg">
+  </form>
   <form action="account_list.php" method="GET">
     <input id="search" type="text" placeholder="Søg her">
     <input id="submit" type="submit" value="Search">
+  </form>
   <form action="notification.php">
     <input id="notification" type="image" src="img/notification.jpg">
+  </form>
+  <form action="emp_page.php">
+    <input id="notification" type="image" src="img/profile.jpg">
+  </form>
 </header>
