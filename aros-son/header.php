09e292f6525f6949d8d7282068c559292ba28d21
NicolasPenon <73479527+NicolasPenon@users.noreply.github.com>
2021-05-25 21:55:09 +0200

    opdateret hele lortet

--- aros-son/header.php
+++ aros-son/header.php
@@ -1,16 +1,19 @@
+<link rel="stylesheet" href="css/design.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <header>
-  <link rel="stylesheet" href="css/design.css">
-  <form action="menu.php">
-    <input id="menu" type="image" src="img/menu.jpg">
-  </form>
-  <form action="account_list.php" method="GET">
-    <input id="search" type="text" placeholder="Søg her">
-    <input id="submit" type="submit" value="Search">
-  </form>
-  <form action="notification.php">
-    <input id="notification" type="image" src="img/notification.jpg">
-  </form>
-  <form action="emp_page.php">
-    <input id="notification" type="image" src="img/profile.jpg">
-  </form>
+  <div id="div_menu">
+      <a id="menu" href="menu.php"><img src="img/menu.png"></a>
+  </div>
+  <div id="div_search">
+    <form action="account_list.php" method="GET">
+      <input id="search" type="text" placeholder="Søg her">
+      <input id="submit" type="submit" value="Søg">
+    </form>
+  </div>
+  <div id="div_notif">
+      <a id="notification" href="notification.php"><img src="img/notification.png" onclick="notification.php"></a>
+  </div>
+  <div id="div_profile">
+      <a id="profile" href="emp_page.php"><img src="img/profile.png" onclick="emp_page.php"></a>
+  </div>
 </header>
