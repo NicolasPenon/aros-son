f574bd9af4de528f6a6efdd093b3c8af20d7740b
Markdalin <Mdpe27852@edu.ucl.dk>
2021-05-25 21:43:05 +0200

    emp css

--- aros-son/emp_page.php
+++ aros-son/emp_page.php
@@ -2,25 +2,60 @@
  <html lang="en" dir="ltr">
    <head>
      <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      <title></title>
+     <link rel="stylesheet" href="css/emp.css">
    </head>
    <body>
-     <center>
+     <?php
+     include('header.php');
+      ?>
+
+         <div id="box1">
+           <img src="img/profile.jpg" class="pb"
+           <img align="left">
        <h1> Medarbejder 1</h1>
        <p> Account Manager
          <br>
          Tlf 20202020
       </p>
+    </div>
+
+  <div id="box2">
+    <center>
+      Aktiviteter
+    </center>
+  </div>
+
+    <div id="box3">
+      @Medarbejder1 har genforhanldet 2-årig kontrakt med Kunde1 </p>
+    </div>
+
+    <div id="box4">
+      @Medarbejder2 <br>
+      Hej @Medarbejder1, Kunde1 ønsker at blive kontaktet, tag fat i Gustav
+    </div>
+
+    <div id="box5">
+      @Medarbejder1 <br>
+      Hej @Medarbejder3, til info så er Kunde3 utilfreds med nuværende løsning
+    </div>
+
+    <div id="box6">
+      @Gruppe3 <br>
+      En begivenhed er blevet tilføjet til d.30/6-21 (sommerfest)
+    </div>
+
+    <div id="box7">
+      @Medarbejder2 <br>
+      Random fyld ud shit her. En reference til en anden medarbejder eller tilfældig kunde
+    </div>
 
 
 
-       <h1>Aktiviteter</h1>
 
 
 
 
-
-     <div class="container">
-       <div id="upperbox">
    </body>
  </html>
