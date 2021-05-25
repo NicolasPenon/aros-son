f1ee7ffe3a58a74a0f17425f7c2ea7f435ee13d5
NicolasPenon <73479527+NicolasPenon@users.noreply.github.com>
2021-05-25 14:52:35 +0200

    mere

--- aros-son/header.php
+++ aros-son/header.php
@@ -1,7 +1,9 @@
 <header>
   <form action="menu.php">
-    <input id="menu" type="button" value="">
+    <input id="menu" type="image" src="img/menu.jpg">
   <form action="account_list.php" method="GET">
     <input id="search" type="text" placeholder="Søg her">
     <input id="submit" type="submit" value="Search">
+  <form action="notification.php">
+    <input id="notification" type="image" src="img/notification.jpg">
 </header>
