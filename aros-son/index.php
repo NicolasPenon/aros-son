5baa8519636c275ed811c38acd1b7f8c0995754d
NicolasPenon <73479527+NicolasPenon@users.noreply.github.com>
2021-05-25 13:54:24 +0200

    lidt mere

---
+++ aros-son/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aros & Son CRM</title>
+  </head>
+
+
+  <body>
+    <header>
+    </header>
+    
+  </body>
+</html>
