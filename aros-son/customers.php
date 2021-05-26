a905693b89f8f0dea0b47fb7bf3d2ea8030931a6
Markdalin <Mdpe27852@edu.ucl.dk>
2021-05-26 22:17:52 +0200

    customers update

---
+++ aros-son/customers.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title>
+    <link rel="stylesheet" href="css/customers.css">
+  </head>
+  <body>
+    <?php
+    include('header.php');
+    include('functions.php');
+    ?>
+
+    <div id="kunder">
+      <h1>Kunder</h1>
+    </div>
+
+    <div id="checkboxes">
+      <form method="post" action="/test/">
+      <fieldset>
+      <input type="checkbox" name="group" value="Alle">
+      <label for="Alle">Alle</label>
+      <input type="checkbox" name="group" value="Enterprise">
+      <label for="Alle">Enterprise</label>
+      <input type="checkbox" name="group" value="Large">
+      <label for="Alle">Large</label>
+      <br>
+      <input type="checkbox" name="group" value="Medium">
+      <label for="Alle">Medium</label>
+      <input type="checkbox" name="group" value="Small">
+      <label for="Alle">Small</label>
+    </fieldset>
+  </div>
+
+<div class="container">
+
+    <div id="customer1">
+      ssssss
+    </div>
+
+    <div id="customer2">
+      sssss
+    </div>
+
+    <div id="customer3">
+      sssss
+    </div>
+
+    <div id="customer4">
+      ssss
+    </div>
+
+    <div id="customer5">
+          ssss
+    </div>
+
+    <div id="customer6">
+          ssss
+    </div>
+
+    <div id="customer7">
+          ssss
+    </div>
+
+    <div id="customer8">
+          ssss
+    </div>
+
+    <div id="customer9">
+          ssss
+    </div>
+
+    <div id="customer10">
+          ssss
+    </div>
+
+    <div id="customer11">
+          ssss
+    </div>
+
+    <div id="customer12">
+          ssss
+    </div>
+
+    <div id="customer13">
+          ssss
+    </div>
+
+    <div id="customer14">
+          ssss
+    </div>
+
+    <div id="customer15">
+          ssss
+    </div>
+
+    <div id="customer16">
+          ssss
+    </div>
+
+    </div>
+  </body>
+</html>
--- aros-son/frontpage.php
+++ aros-son/frontpage.php
@@ -6,6 +6,7 @@ include('functions.php');
 <html lang="en" dir="ltr">
   <head>
 <link rel="stylesheet" href="css/frontpage.css">
+ <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Forside</title>
   </head>
